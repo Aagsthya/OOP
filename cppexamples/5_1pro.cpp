13a92b216ee96183d5821db7d113ff3fb1306350
Aagsthya <101513454+Aagsthya@users.noreply.github.com>
2022-10-13 20:10:48 +0530

    Create 5_1pro.cpp

---
+++ cppexamples/5_1pro.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class items
+{
+	int number; 
+	float cost; 
+ public:
+	void getdata(int a , float b );	 
+	                                 
+	void putdata(void)
+	{
+		cout << "number :"<< number <<"\n";
+		cout << "cost :"<< cost << "\n";
+	 } 
+};
+
+void items :: getdata(int a , float b) 
+{
+	number = a;
+	cost = b;	
+}  
+int main()
+{
+	items x;
+	
+	cout <<"\nobject x"<<"\n";
+	
+	x.getdata(100, 299.89);
+	x.putdata();
+	
+	items y;
+	
+	cout << "\nobject y" << "\n";
+	
+	y.getdata(200, 175.50);
+	y.putdata();
+	
+	return 0;
+}
